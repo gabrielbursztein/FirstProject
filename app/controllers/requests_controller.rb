d10b401a14fd1fd4265b48b7cc7b6fe4fc507a63
gabrielbursztein <gbursztein@toptierlabs.com>
2017-04-11 12:42:44 -0300

    css books on shelf

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,12 +1,11 @@
 class RequestsController < ApplicationController
 
   def index
-    @requests = current_user.requests
+    user_signed_in? ? @requests = current_user.requests : redirect_to(new_user_session_path)
   end
 
   def create
     @book = Book.find(params[:book_id])
     @request = @book.requests.create(user_id: current_user.id)
   end
-
 end
