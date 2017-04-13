cf766b2e7f730d2081377baccb24ae3f9a08ee6c
gabrielbursztein <gbursztein@toptierlabs.com>
2017-04-13 15:17:57 -0300

    Comments now have css

--- db/migrate/20170405191617_create_comments.rb
+++ db/migrate/20170405191617_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.string :name
       t.integer :rate
+      t.references :user
       t.string :description
       t.references :book
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406162032) do
+ActiveRecord::Schema.define(version: 20170417182911) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -22,13 +22,14 @@ ActiveRecord::Schema.define(version: 20170406162032) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string   "name"
     t.integer  "rate"
+    t.integer  "user_id"
     t.string   "description"
     t.integer  "book_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["book_id"], name: "index_comments_on_book_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "requests", force: :cascade do |t|
@@ -55,6 +56,7 @@ ActiveRecord::Schema.define(version: 20170406162032) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
